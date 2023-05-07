7782680219399a58383e3db46f307c3b66733049
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2023-05-07 11:58:34 +0530

    Basic behaviour done

---
+++ src/Pages/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import Hero from '../components/Hero'
+import ProductCard from '../components/ProductCard'
+import Stats from '../components/Stats'
+import Categories from '../components/Categories'
+
+const Home = () => {
+    
+    const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        const fetchProducts = async ()=>{
+            const res = await fetch('https://fakestoreapi.com/products?limit=12')
+            const data = await res.json()
+            console.log(data)
+            setProducts(data)
+        }
+        fetchProducts()
+    },[])
+
+  return (
+    <>
+      <Hero />
+      <Categories/>
+      {
+        products.length > 0 ? <ProductCard products={products}/> : null
+      }
+    </>
+  );
+}
+
+export default Home
